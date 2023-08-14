85d37beaf44ca10d3c6c9d7b8f2f62bcb32c6752
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-08-14 20:45:40 +0200

    first version of the database: user, event & category tables created

--- migrations/deploy/1.create_tables.sql
+++ migrations/deploy/1.create_tables.sql
@@ -2,6 +2,37 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+-- Création de la table "Utilisateur"
+CREATE TABLE IF NOT EXISTS "user" (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email text NOT NULL UNIQUE,
+    firstname text NOT NULL,
+    lastname text NOT NULL,
+    age int NOT NULL,
+    password text NOT NULL,
+    localization text NOT NULL,
+    level int,
+    experience int,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ
+);
+
+-- Création de la table "Catégorie"
+CREATE TABLE IF NOT EXISTS "category" (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    label TEXT NOT NULL UNIQUE
+)
+
+-- Création de la table "Evenement"
+CREATE TABLE IF NOT EXISTS "event" (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title text NOT NULL,
+    description text NOT NULL,
+    date TIMESTAMPTZ NOT NULL,
+    hour TIMESTAMPTZ NOT NULL,
+    location text NOT NULL,
+    category_id int REFERENCES category(id),
+    organizer_id int REFERENCES user(id)
+);
 
 COMMIT;
