c340b36d64f20283f99005dd88c17338472929c8
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-08-16 00:47:27 +0200

    user model created

---
+++ app/models/User.js
@@ -0,0 +1,43 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+class User extends Model { }
+
+User.init({
+    email: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true
+    },
+    firstname: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    level: {
+        type: DataTypes.INTEGER,
+    },
+    experience: {
+        type: DataTypes.INTEGER,
+    },
+    isAdmin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+    },
+
+},
+    {
+        sequelize,
+        modelName: 'User',
+        tableName: 'user',
+    });
+
+export default User;
