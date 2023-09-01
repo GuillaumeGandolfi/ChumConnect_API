f237b15f6af980d055864db38c95b1ca46c53dc3
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-09-01 17:24:35 +0200

    create_tables updated, index and default values added

--- migrations/deploy/1.create_tables.sql
+++ migrations/deploy/1.create_tables.sql
@@ -2,6 +2,8 @@
 
 BEGIN;
 
+----- CREATION DES TABLES -----
+
 -- Table des utilisateurs du système
 CREATE TABLE IF NOT EXISTS "user" (
     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -11,9 +13,9 @@ CREATE TABLE IF NOT EXISTS "user" (
     age int NOT NULL COMMENT 'Âge de l''utilisateur',
     password text NOT NULL COMMENT 'Mot de passe hashé de l''utilisateur',
     city text NOT NULL COMMENT 'Ville de résidence de l''utilisateur',
-    level int COMMENT 'Niveau de l''utilisateur (pour un jeu ou une compétence)',
-    experience int COMMENT 'Expérience de l''utilisateur (pour un jeu ou une compétence)',
-    isAdmin BOOLEAN DEFAULT FALSE COMMENT 'Indique si l''utilisateur a un rôle d''administrateur',
+    level int NOT NULL DEFAULT 0 COMMENT 'Niveau de l''utilisateur (pour un jeu ou une compétence)',
+    experience int NOT NULL DEFAULT 0 COMMENT 'Expérience de l''utilisateur (pour un jeu ou une compétence)',
+    is_admin BOOLEAN DEFAULT FALSE COMMENT 'Indique si l''utilisateur a un rôle d''administrateur',
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW() COMMENT 'Date et heure de création du compte utilisateur',
     updated_at TIMESTAMPTZ COMMENT 'Date et heure de la dernière mise à jour du compte utilisateur'
 );
@@ -34,16 +36,16 @@ CREATE TABLE IF NOT EXISTS "event" (
     date DATE NOT NULL COMMENT 'Date de l''événement',
     start_time TIME NOT NULL COMMENT 'Heure de début de l''événement',
     location text NOT NULL COMMENT 'Lieu de l''événement (ex: nom du bar, parc)',
-    category_id int REFERENCES category(id) COMMENT 'Référence à la catégorie de l''événement',
-    organizer_id int REFERENCES "user"(id) COMMENT 'Identifiant de l''organisateur de l''événement',
+    category_id int NOT NULL REFERENCES category(id) COMMENT 'Référence à la catégorie de l''événement',
+    organizer_id int NOT NULL REFERENCES "user"(id) COMMENT 'Identifiant de l''organisateur de l''événement',
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW() COMMENT 'Date et heure de création de l''événement',
     updated_at TIMESTAMPTZ COMMENT 'Date et heure de la dernière mise à jour de l''événement'
 );
 
 -- Table d'association pour les amis des utilisateurs
 CREATE TABLE IF NOT EXISTS "user_has_friend" (
-    user_id int REFERENCES "user"(id) COMMENT 'Identifiant de l''utilisateur',
-    friend_id int REFERENCES "user"(id) COMMENT 'Identifiant de l''ami de l''utilisateur',
+    user_id int NOT NULL REFERENCES "user"(id) COMMENT 'Identifiant de l''utilisateur',
+    friend_id int NOT NULL REFERENCES "user"(id) COMMENT 'Identifiant de l''ami de l''utilisateur',
     date TIMESTAMPTZ NOT NULL DEFAULT NOW() COMMENT 'Date et heure de l''ajout de l''ami',
     PRIMARY KEY (user_id, friend_id),
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW() COMMENT 'Date et heure de création de l''amitié',
@@ -51,9 +53,9 @@ CREATE TABLE IF NOT EXISTS "user_has_friend" (
 );
 
 -- Table d'association pour les demandes d'amis entre utilisateurs
-CREATE TABLE IF NOT EXISTS "friend_requests" (
-    sender_id int REFERENCES "user"(id) COMMENT 'Identifiant de l''utilisateur envoyant la demande',
-    receiver_id int REFERENCES "user"(id) COMMENT 'Identifiant de l''utilisateur recevant la demande',
+CREATE TABLE IF NOT EXISTS "friend_request" (
+    sender_id int NOT NULL REFERENCES "user"(id) COMMENT 'Identifiant de l''utilisateur envoyant la demande',
+    receiver_id int NOT NULL REFERENCES "user"(id) COMMENT 'Identifiant de l''utilisateur recevant la demande',
     PRIMARY KEY (sender_id, receiver_id),
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW() COMMENT 'Date et heure de l''envoi de la demande',
     updated_at TIMESTAMPTZ COMMENT 'Date et heure de la dernière mise à jour de la demande'
@@ -61,8 +63,8 @@ CREATE TABLE IF NOT EXISTS "friend_requests" (
 
 -- Table d'association pour les participants d'un événement
 CREATE TABLE IF NOT EXISTS "event_has_participant" (
-    event_id int REFERENCES event(id) COMMENT 'Identifiant de l''événement',
-    participant_id int REFERENCES "user"(id) COMMENT 'Identifiant du participant',
+    event_id int NOT NULL REFERENCES event(id) COMMENT 'Identifiant de l''événement',
+    participant_id int NOT NULL REFERENCES "user"(id) COMMENT 'Identifiant du participant',
     PRIMARY KEY (event_id, participant_id),
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW() COMMENT 'Date et heure de la participation',
     updated_at TIMESTAMPTZ COMMENT 'Date et heure de la dernière mise à jour de la participation'
@@ -70,12 +72,19 @@ CREATE TABLE IF NOT EXISTS "event_has_participant" (
 
 -- Table d'association pour les invitations à un événement
 CREATE TABLE IF NOT EXISTS "event_invitation" (
-    event_id int REFERENCES event(id) COMMENT 'Identifiant de l''événement',
-    organizer_id int REFERENCES "user"(id) COMMENT 'Identifiant de l''organisateur de l''événement',
-    invited_friend_id int REFERENCES "user"(id) COMMENT 'Identifiant de l''ami invité',
+    event_id int NOT NULL REFERENCES event(id) COMMENT 'Identifiant de l''événement',
+    organizer_id int NOT NULL REFERENCES "user"(id) COMMENT 'Identifiant de l''organisateur de l''événement',
+    invited_friend_id int NOT NULL REFERENCES "user"(id) COMMENT 'Identifiant de l''ami invité',
     PRIMARY KEY (event_id, organizer_id, invited_friend_id),
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW() COMMENT 'Date et heure de l''envoi de l''invitation',
     updated_at TIMESTAMPTZ COMMENT 'Date et heure de la dernière mise à jour de l''invitation'
 );
+
+
+----- CREATION DES INDEX -----
+CREATE INDEX idx_user_email ON "user"(email);
+CREATE INDEX idx_event_organizer ON event(organizer_id);
+CREATE INDEX idx_event_participant ON event_has_participant(event_id);
+CREATE INDEX idx_event_invitation ON event_invitation(event_id);
 
 COMMIT;
